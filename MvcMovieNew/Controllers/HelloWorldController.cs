9bd6da51d0af080d915c5a873fdc1ad9bbe7daa8
Aleksandr Kougia <zero0072002@mail.ru>
2019-09-05 18:56:34 +0300

    first commit

---
+++ MvcMovieNew/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovieNew.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome
+        //public string Welcome()
+        //{
+        //    return "This is the welcome action method!...";
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 0)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ MvcMovieNew/Data/MvcMovieNewContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovieNew.Models
+{
+    public class MvcMovieNewContext : DbContext
+    {
+        public MvcMovieNewContext (DbContextOptions<MvcMovieNewContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieNew.Models.Movie> Movie { get; set; }
+    }
+}
